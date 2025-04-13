796f108bdf13813a09cd4cd316a2b19c3fd55d6b
Lakshya-23 <Lakshya_Varshney@Outlook.com>
2025-04-13 22:39:19 +0530

    removed name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -125,10 +125,20 @@ function App() {
                         <option value="PG">Postgraduate (PG)</option>
                         <option value="PHD">Doctorate (PhD)</option>
                       </select>
+
+                      {/* Left icon */}
                       <div className="absolute left-0 inset-y-0 flex items-center pl-3 pointer-events-none">
                         <FaGraduationCap className="h-5 w-5 text-gray-400" />
                       </div>
+
+                      {/* Dropdown arrow icon (right side) */}
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                        <svg className="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                          <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+                        </svg>
+                      </div>
                     </div>
+
                   </div>
 
                   <button
